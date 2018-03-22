5213522d1fcfe8b30046f6ae0de331cca777629f
Leonardo Geus <leonardodegeus@gmail.com>
2018-03-21 23:06:26 -0300

    Created some python code to pre process images and predict using a CNN.

--- cnn.py
+++ cnn.py
@@ -17,21 +17,41 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Activation, Dropout
+from keras import backend as K
+
+epochs = 50
+batch_size = 16
+
+img_width, img_height = 257, 170
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
 
 classifier = Sequential()
-classifier.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-classifier.add(Conv2D(64, (3, 3), activation='relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape=input_shape))
+classifier.add(Activation('relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Dropout(0.25))
-classifier.add(Flatten())
-classifier.add(Dense(128, activation='relu'))
-classifier.add(Dropout(0.5))
-classifier.add(Dense(num_classes, activation='softmax'))
 
-classifier.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
+classifier.add(Conv2D(32, (3, 3)))
+classifier.add(Activation('relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+classifier.add(Conv2D(64, (3, 3)))
+classifier.add(Activation('relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+classifier.add(Flatten())
+classifier.add(Dense(64))
+classifier.add(Activation('relu'))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(1))
+classifier.add(Activation('sigmoid'))
+
+classifier.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
               metrics=['accuracy'])
 
 from keras.preprocessing.image import ImageDataGenerator
@@ -43,35 +63,26 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (64, 64),
-                                                 batch_size = 32,
+training_set = train_datagen.flow_from_directory('ProcessedImage/training_set',
+                                                 target_size = (img_width, img_height),
+                                                 batch_size = batch_size,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size = (64, 64),
-                                            batch_size = 32,
+test_set = test_datagen.flow_from_directory('ProcessedImage/test_set',
+                                            target_size = (img_width, img_height),
+                                            batch_size = batch_size,
                                             class_mode = 'binary')
 
-classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000)
+nb_train_samples = len(training_set)
+nb_validation_samples = len(test_set)
 
-score = classifier.evaluate(training_set)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+t1 = nb_train_samples // batch_size
+t2 = nb_validation_samples // batch_size
 
-import numpy as np
-from keras.preprocessing import image
-
-test_image = image.load_img("dataset/single_prediction/cat_or_dog_1.jpg", target_size = (64,64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-training_set.class_indices
-if result[0][0] == 1 :
-    prediction = "dog"
-else:
-    prediction = "cat"
+classifier.fit_generator(
+                    training_set,
+                    steps_per_epoch=t1,
+                    epochs=epochs,
+                    validation_data=test_set,
+                    validation_steps=t2)
+model.save_weights(save_path)
